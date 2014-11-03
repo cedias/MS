c10dc4a627cc6bd67dac4bb1e3d72bece57c5261
Charles-Emmanuel Dias <Charles-Emmanuel.dias@etu.upmc.fr>
2014-11-03 18:21:00 +0100

    ok

--- Hadoop/src/WordCount.java
+++ Hadoop/src/WordCount.java
@@ -5,8 +5,8 @@ import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
-import org.apache.hadoop.mapred.FileInputFormat;
 import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 
@@ -26,7 +26,15 @@ public class WordCount {
 			job.setOutputValueClass(IntWritable.class);
 			FileInputFormat.addInputPath(job, new Path(args[0]));
 			FileOutputFormat.setOutputPath(job, new Path(args[1]));
-			job.waitForCompletion(true);
+			try {
+				job.waitForCompletion(true);
+			} catch (ClassNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 			
 		} catch (IOException e) {
 			
--- Hadoop/src/WordCountMapper.java
+++ Hadoop/src/WordCountMapper.java
@@ -5,6 +5,7 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
 
+
 public class WordCountMapper extends Mapper<Object, Text, Text, IntWritable> {
 
 	@Override
